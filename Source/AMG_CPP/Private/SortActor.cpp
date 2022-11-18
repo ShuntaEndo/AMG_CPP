d4fc1cb3fce50ec9230c3d93c2e2c22e04430826
s-endo <s-endo@leon-game.com>
2022-11-18 15:51:31 +0900

    課題実装例

--- Source/AMG_CPP/Private/SortActor.cpp
+++ Source/AMG_CPP/Private/SortActor.cpp
@@ -1,4 +1,5 @@
 #include "SortActor.h"
+#include "Kismet/KismetArrayLibrary.h"
 
 ASortActor::ASortActor()
 {
@@ -8,10 +9,16 @@ ASortActor::ASortActor()
 
 void ASortActor::Reset()
 {
+	IntArray.Empty();
+	IntArray.AddUninitialized(Num);
 	
+	for (int32 i = 0; i < IntArray.Num(); i++)
+	{
+		IntArray[i] = FMath::RandRange(RandomMin, RandomMax);
+	}
 }
 
 void ASortActor::Sort()
 {
-	
+	IntArray.Sort();
 }
