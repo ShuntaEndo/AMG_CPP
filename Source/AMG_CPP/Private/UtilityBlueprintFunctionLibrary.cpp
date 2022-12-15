2c0a6bb30357485e563771a2f5edea328fa45e30
s-endo <s-endo@leon-game.com>
2022-12-15 20:25:34 +0900

    課題追加

---
+++ Source/AMG_CPP/Private/UtilityBlueprintFunctionLibrary.cpp
@@ -0,0 +1,1 @@
+#include "UtilityBlueprintFunctionLibrary.h"
---
+++ Source/AMG_CPP/Public/UtilityBlueprintFunctionLibrary.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UtilityBlueprintFunctionLibrary.generated.h"
+
+UCLASS()
+class AMG_CPP_API UUtilityBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
