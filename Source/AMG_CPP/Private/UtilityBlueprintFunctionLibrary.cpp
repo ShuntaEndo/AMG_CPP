089a47750453795a8fe8acd6bc2350f93bbe5e3f
s-endo <s-endo@leon-game.com>
2022-12-16 15:33:06 +0900

    課題実装例

--- Source/AMG_CPP/Private/UtilityBlueprintFunctionLibrary.cpp
+++ Source/AMG_CPP/Private/UtilityBlueprintFunctionLibrary.cpp
@@ -1,1 +1,20 @@
 #include "UtilityBlueprintFunctionLibrary.h"
+
+bool UUtilityBlueprintFunctionLibrary::OpenSaveFolder()
+{
+	FString SavePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectSavedDir());
+	if (FPaths::DirectoryExists(SavePath))
+	{
+		FPlatformProcess::ExploreFolder(*SavePath);
+		return true;
+	}
+	return false;
+}
+
+bool UUtilityBlueprintFunctionLibrary::CreateTxtFile(const FText& Text)
+{
+	FString Filename = FString::Printf(TEXT("%s.txt"), *FDateTime::Now().ToString(TEXT("%Y%m%d_%H%M%S")));
+    FString SavePath = FPaths::ConvertRelativePathToFull(FPaths::ProjectSavedDir());
+
+    return FFileHelper::SaveStringToFile(Text.ToString(), *FString(SavePath + "/" + Filename), FFileHelper::EEncodingOptions::ForceUTF8);
+}
--- Source/AMG_CPP/Public/UtilityBlueprintFunctionLibrary.h
+++ Source/AMG_CPP/Public/UtilityBlueprintFunctionLibrary.h
@@ -8,4 +8,11 @@ UCLASS()
 class AMG_CPP_API UUtilityBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static bool OpenSaveFolder();
+
+	UFUNCTION(BlueprintCallable)
+	static bool CreateTxtFile(const FText& Text);
 };
